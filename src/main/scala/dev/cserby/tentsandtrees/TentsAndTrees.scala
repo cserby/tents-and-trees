fb580aba3a97a176ea14c4d41b3c9f10bb409a13
Márton Cserbák <mcserbak@hiya.com>
2019-03-18 22:15:38 +0100

    More difficult puzzle

--- src/main/scala/dev/cserby/tentsandtrees/TentsAndTrees.scala
+++ src/main/scala/dev/cserby/tentsandtrees/TentsAndTrees.scala
@@ -3,15 +3,40 @@ package dev.cserby.tentsandtrees
 import oscar.cp.{CPIntVar, CPModel, Constraint, add, binaryFirstFail, search, start}
 
 object TentsAndTrees extends CPModel with App {
-  val inputTable: Array[Array[Char]] = Array(
+/*  val inputTable: Array[Array[Char]] = Array(
     Array('t', ' ', ' '),
     Array(' ', ' ', ' '),
     Array(' ', ' ', 't')
   )
 
   val rowSums: Array[Int] = Array(0, 2, 0)
-  val colSums: Array[Int] = Array(1, 0, 1)
+  val colSums: Array[Int] = Array(1, 0, 1)*/
 
+  val rowSums: Array[Int] = Array(6, 4, 3, 6, 2, 6, 1, 5, 3, 4, 5, 3, 3, 4, 3, 4, 2, 7, 1, 8)
+  val colSums: Array[Int] = Array(4, 5, 2, 6, 2, 5, 4, 5, 2, 5, 4, 3, 5, 4, 5, 3, 3, 4, 3, 6)
+
+  val inputTable: Array[Array[Char]] = Array(
+    " t   tt    tt   t   ".toCharArray,
+    "tt            t    t".toCharArray,
+    "     t  t         t ".toCharArray,
+    "   t    t tt       t".toCharArray,
+    " t    t       t  t  ".toCharArray,
+    "  t       t t t     ".toCharArray,
+    "          t   tt   t".toCharArray,
+    " t    t         t   ".toCharArray,
+    " t  t       t      t".toCharArray,
+    "    t  t  tt  t     ".toCharArray,
+    "             tt t   ".toCharArray,
+    "      tt          t ".toCharArray,
+    " t  t    t          ".toCharArray,
+    " t  t               ".toCharArray,
+    " t   t     t  t t   ".toCharArray,
+    " t     t t          ".toCharArray,
+    "         t  tt   t t".toCharArray,
+    " tt    t            ".toCharArray,
+    "     tt   t t t t  t".toCharArray,
+    "  t     t      t t  ".toCharArray
+  )
   def addConstraints(table: Table): Unit = {
     add(table.edgeConstraints)
     add(table.tentConstraints)
