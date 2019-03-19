198b319ddf23eba8508b715aac5de88f123763c3
Márton Cserbák <mcserbak@hiya.com>
2019-03-19 14:48:01 +0100

    Tests instead of App

--- build.sbt
+++ build.sbt
@@ -9,3 +9,5 @@ resolvers += Resolver.url("typesafe", url("http://repo.typesafe.com/typesafe/rel
 resolvers += "Oscar Releases" at "http://artifactory.info.ucl.ac.be/artifactory/libs-release-local/"
 
 libraryDependencies += "oscar" %% "oscar-cp" % "4.0.0" withSources()
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.5"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
---
+++ src/test/scala/dev/cserby/tentsandtrees/AcceptanceTests.scala
@@ -0,0 +1,78 @@
+package dev.cserby.tentsandtrees
+
+import org.scalatest.{FunSpec, Matchers}
+
+class AcceptanceTests extends FunSpec with Matchers {
+  describe("Tents and Trees solver") {
+    it("3x3 easy") {
+      println(TentsAndTreesPuzzle(
+        Array(
+          Array('t', ' ', ' '),
+          Array(' ', ' ', ' '),
+          Array(' ', ' ', 't')
+        ),
+        Array(0, 2, 0),
+        Array(1, 0, 1)
+      ).solve)
+    }
+
+    it("8x8 easy") {
+      println(TentsAndTreesPuzzle(
+        Array(
+          " t      ",
+          "   t  t ",
+          "   t   t",
+          "        ",
+          "    t t ",
+          "t  t   t",
+          "      t ",
+          "t       "
+        ).map(_.toCharArray),
+        Array(1, 3, 0, 1, 3, 1, 2, 1),
+        Array(1, 2, 1, 2, 0, 3, 0, 3)
+      ).solve)
+    }
+
+    it("20x20 hard") {
+      println(TentsAndTreesPuzzle(
+        Array(
+          " t   tt    tt   t   ",
+          "tt            t    t",
+          "     t  t         t ",
+          "   t    t tt       t",
+          " t    t       t  t  ",
+          "  t       t t t     ",
+          "          t   tt   t",
+          " t    t         t   ",
+          " t  t       t      t",
+          "    t  t  tt  t     ",
+          "             tt t   ",
+          "      tt          t ",
+          " t  t    t          ",
+          " t  t               ",
+          " t   t     t  t t   ",
+          " t     t t          ",
+          "         t  tt   t t",
+          " tt    t            ",
+          "     tt   t t t t  t",
+          "  t     t      t t  "
+        ).map(_.toCharArray),
+        Array(6, 4, 3, 6, 2, 6, 1, 5, 3, 4, 5, 3, 3, 4, 3, 4, 2, 7, 1, 8),
+        Array(4, 5, 2, 6, 2, 5, 4, 5, 2, 5, 4, 3, 5, 4, 5, 3, 3, 4, 3, 6)
+      ).solve)
+    }
+
+    it("tent conflict debug") {
+      println(TentsAndTreesPuzzle(
+        Array(
+          "   t   t",
+          "        ",
+          "    t t ",
+          "t  t   t"
+        ).map(_.toCharArray),
+        Array(0, 1, 3, 1),
+        Array(1, 0, 1, 1, 0, 1, 0, 3)
+      ).solve)
+    }
+  }
+}
