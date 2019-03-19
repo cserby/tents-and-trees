035b7c8f1656d1703a8597f02ff6c72c6b36d97b
Márton Cserbák <mcserbak@hiya.com>
2019-03-19 14:31:51 +0100

    New puzzle

--- src/main/scala/dev/cserby/tentsandtrees/TentsAndTrees.scala
+++ src/main/scala/dev/cserby/tentsandtrees/TentsAndTrees.scala
@@ -10,7 +10,7 @@ object TentsAndTrees extends App {
   val rowSums: Array[Int] = Array(0, 2, 0)
   val colSums: Array[Int] = Array(1, 0, 1)*/
 
-  val rowSums: Array[Int] = Array(6, 4, 3, 6, 2, 6, 1, 5, 3, 4, 5, 3, 3, 4, 3, 4, 2, 7, 1, 8)
+  /*val rowSums: Array[Int] = Array(6, 4, 3, 6, 2, 6, 1, 5, 3, 4, 5, 3, 3, 4, 3, 4, 2, 7, 1, 8)
   val colSums: Array[Int] = Array(4, 5, 2, 6, 2, 5, 4, 5, 2, 5, 4, 3, 5, 4, 5, 3, 3, 4, 3, 6)
 
   val inputTable: Array[Array[Char]] = Array(
@@ -34,6 +34,20 @@ object TentsAndTrees extends App {
     " tt    t            ",
     "     tt   t t t t  t",
     "  t     t      t t  "
+  ).map(_.toCharArray)*/
+
+  val rowSums: Array[Int] = Array(1, 3, 0, 1, 3, 1, 2, 1)
+  val colSums: Array[Int] = Array(1, 2, 1, 2, 0, 3, 0, 3)
+
+  val inputTable: InputTable = Array(
+    " t      ",
+    "   t  t ",
+    "   t   t",
+    "        ",
+    "    t t ",
+    "t  t   t",
+    "      t ",
+    "t       "
   ).map(_.toCharArray)
 
   println(TentsAndTreesPuzzle(inputTable, rowSums, colSums).solve)
