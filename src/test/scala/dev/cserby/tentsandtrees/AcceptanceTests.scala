9be3ef8cba49f170de8e00f861004bd7f1ef4d4d
Márton Cserbák <mcserbak@hiya.com>
2019-03-19 21:18:45 +0100

    More tests

--- src/test/scala/dev/cserby/tentsandtrees/AcceptanceTests.scala
+++ src/test/scala/dev/cserby/tentsandtrees/AcceptanceTests.scala
@@ -61,5 +61,96 @@ class AcceptanceTests extends FunSpec with Matchers {
         Array(4, 5, 2, 6, 2, 5, 4, 5, 2, 5, 4, 3, 5, 4, 5, 3, 3, 4, 3, 6)
       ).solve)
     }
+
+    it("TentsAndTrees C - 13x13") {
+      println(TentsAndTreesPuzzle(
+        Array(
+          "t          t ",
+          "t t t    tt  ",
+          "      t     t",
+          "         t t ",
+          "    t   t  t ",
+          " t t      t  ",
+          "        t  t ",
+          " t  t t     t",
+          "t            ",
+          "    t  tt    ",
+          "     t       ",
+          "    t  t t  t",
+          " t   t    t  "
+        ).map(_.toCharArray),
+        Array(2, 4, 2, 4, 2, 2, 4, 2, 2, 3, 2, 2, 4),
+        Array(2, 4, 0, 4, 2, 3, 2, 2, 4, 2, 4, 1, 5)
+      ).solve)
+    }
+
+    it("TentsAndTrees C - 13x13 - 2") {
+      println(TentsAndTreesPuzzle(
+        Array(
+          "             ",
+          "  t    t t  t",
+          "t     tt     ",
+          "   t t     t ",
+          "         t   ",
+          "t    t    t  ",
+          "  t       t  ",
+          "      t     t",
+          "t tt      t  ",
+          "      t    t ",
+          " t  t t  t   ",
+          "      t   tt ",
+          "  t t t      "
+        ).map(_.toCharArray),
+        Array(1, 5, 0, 5, 1, 3, 2, 3, 2, 3, 3, 2, 4),
+        Array(3, 2, 3, 3, 3, 2, 2, 3, 1, 3, 3, 2, 4)
+      ).solve)
+    }
+
+    it("TentsAndTrees C - 13x13 - 3") {
+      println(TentsAndTreesPuzzle(
+        Array(
+          " t  t        ",
+          "     t t    t",
+          "      t  t t ",
+          " t         t ",
+          " t  t  tt    ",
+          "    t     t t",
+          " t  tt       ",
+          "t     t     t",
+          "  tt  t  t   ",
+          "             ",
+          "        t   t",
+          "   t    t t  ",
+          " t   t       "
+        ).map(_.toCharArray),
+        Array(4, 2, 1, 5, 2, 4, 2, 4, 1, 3, 1, 2, 3),
+        Array(3, 2, 2, 4, 2, 2, 3, 3, 2, 4, 1, 1, 5)
+      ).solve)
+    }
+
+    it("TentsAndTrees C - 15x15") {
+      println(TentsAndTreesPuzzle(
+        Array(
+          "  t   t    t   ",
+          "t         t  tt",
+          "  t t  t  t t  ",
+          "      t     t t",
+          "               ",
+          "   t     t   t ",
+          " t             ",
+          " t    tt t     ",
+          "  t            ",
+          "       t      t",
+          "t       t t    ",
+          "  t      t    t",
+          "   tt t      t ",
+          " t t     t  tt ",
+          "      t t      "
+        ).map(_.toCharArray),
+        Array(6, 1, 4, 1, 3, 2, 3, 2, 4, 0, 5, 1, 4, 2, 5),
+        Array(4, 2, 3, 3, 1, 4, 1, 6, 0, 3, 3, 3, 2, 3, 5)
+      ).solve)
+    }
+
   }
 }
