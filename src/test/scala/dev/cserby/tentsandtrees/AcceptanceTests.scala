eed05a72ae682b83feb7d784f6f6f4d2a632bf22
Márton Cserbák <mcserbak@hiya.com>
2019-04-15 16:09:32 +0200

    Params by name in AT

--- src/test/scala/dev/cserby/tentsandtrees/AcceptanceTests.scala
+++ src/test/scala/dev/cserby/tentsandtrees/AcceptanceTests.scala
@@ -29,19 +29,19 @@ class AcceptanceTests extends FunSpec with Matchers {
   describe("Tents and Trees solver") {
     it("3x3 easy") {
       tentsAndTreesTest(
-        Array(
+        inputTable = Array(
           Array('t', ' ', ' '),
           Array('T', ' ', 'T'),
           Array(' ', ' ', 't')
         ),
-        Array(0, 2, 0),
-        Array(1, 0, 1)
+        rowSums = Array(0, 2, 0),
+        colSums = Array(1, 0, 1)
       )
     }
 
     it("8x8 easy") {
       tentsAndTreesTest(
-        Array(
+        inputTable = Array(
           " t T    ",
           " T t TtT",
           "   t   t",
@@ -51,14 +51,14 @@ class AcceptanceTests extends FunSpec with Matchers {
           "     TtT",
           "tT      "
         ).map(_.toCharArray),
-        Array(1, 3, 0, 1, 3, 1, 2, 1),
-        Array(1, 2, 1, 2, 0, 3, 0, 3)
+        rowSums = Array(1, 3, 0, 1, 3, 1, 2, 1),
+        colSums = Array(1, 2, 1, 2, 0, 3, 0, 3)
       )
     }
 
     it("20x20 hard") {
       tentsAndTreesTest(
-        Array(
+        inputTable = Array(
           "TtT Ttt   Ttt   tT T",
           "tt    T T   T tT   t",
           " T T t  t         tT",
@@ -80,14 +80,14 @@ class AcceptanceTests extends FunSpec with Matchers {
           " T   tt   t t t t  t",
           "  tT T Tt T T TtTtT "
         ).map(_.toCharArray),
-        Array(6, 4, 3, 6, 2, 6, 1, 5, 3, 4, 5, 3, 3, 4, 3, 4, 2, 7, 1, 8),
-        Array(4, 5, 2, 6, 2, 5, 4, 5, 2, 5, 4, 3, 5, 4, 5, 3, 3, 4, 3, 6)
+        rowSums = Array(6, 4, 3, 6, 2, 6, 1, 5, 3, 4, 5, 3, 3, 4, 3, 4, 2, 7, 1, 8),
+        colSums = Array(4, 5, 2, 6, 2, 5, 4, 5, 2, 5, 4, 3, 5, 4, 5, 3, 3, 4, 3, 6)
       )
     }
 
     it("TentsAndTrees C - 13x13") {
       tentsAndTreesTest(
-        Array(
+        inputTable = Array(
           "tT        Tt ",
           "t tTtT  Ttt T",
           "T     t   T t",
@@ -102,14 +102,14 @@ class AcceptanceTests extends FunSpec with Matchers {
           "   TtT t t  t",
           "Tt   t T TtT "
         ).map(_.toCharArray),
-        Array(2, 4, 2, 4, 2, 2, 4, 2, 2, 3, 2, 2, 4),
-        Array(2, 4, 0, 4, 2, 3, 2, 2, 4, 2, 4, 1, 5)
+        rowSums = Array(2, 4, 2, 4, 2, 2, 4, 2, 2, 3, 2, 2, 4),
+        colSums = Array(2, 4, 0, 4, 2, 3, 2, 2, 4, 2, 4, 1, 5)
       )
     }
 
     it("TentsAndTrees C - 13x13 - 2") {
       tentsAndTreesTest(
-        Array(
+        inputTable = Array(
             "            T",
             "T tT  TtTtT t",
             "t     tt     ",
@@ -124,14 +124,14 @@ class AcceptanceTests extends FunSpec with Matchers {
             "      tT  ttT",
             " TtTtTt   T  "
         ).map(_.toCharArray),
-        Array(1, 5, 0, 5, 1, 3, 2, 3, 2, 3, 3, 2, 4),
-        Array(3, 2, 3, 3, 3, 2, 2, 3, 1, 3, 3, 2, 4)
+        rowSums = Array(1, 5, 0, 5, 1, 3, 2, 3, 2, 3, 3, 2, 4),
+        colSums = Array(3, 2, 3, 3, 3, 2, 2, 3, 1, 3, 3, 2, 4)
       )
     }
 
     it("TentsAndTrees C - 13x13 - 3") {
       tentsAndTreesTest(
-        Array(
+        inputTable = Array(
           " t TtT T    T",
           " T   t t T  t",
           "      t  t tT",
@@ -146,14 +146,14 @@ class AcceptanceTests extends FunSpec with Matchers {
           " T t    t tT ",
           " t T tT T    ",
         ).map(_.toCharArray),
-        Array(4, 2, 1, 5, 2, 4, 2, 4, 1, 3, 1, 2, 3),
-        Array(3, 2, 2, 4, 2, 2, 3, 3, 2, 4, 1, 1, 5)
+        rowSums = Array(4, 2, 1, 5, 2, 4, 2, 4, 1, 3, 1, 2, 3),
+        colSums = Array(3, 2, 2, 4, 2, 2, 3, 3, 2, 4, 1, 1, 5)
       )
     }
 
     it("TentsAndTrees C - 15x15") {
       tentsAndTreesTest(
-        Array(
+        inputTable = Array(
           "T tT Tt   TtT T",
           "t      T  t  tt",
           " Tt t  t TtTtT ",
@@ -170,14 +170,14 @@ class AcceptanceTests extends FunSpec with Matchers {
           "Tt t     t Ttt ",
           "   T TtTtT   T "
         ).map(_.toCharArray),
-        Array(6, 1, 4, 1, 3, 2, 3, 2, 4, 0, 5, 1, 4, 2, 5),
-        Array(4, 2, 3, 3, 1, 4, 1, 6, 0, 3, 3, 3, 2, 3, 5)
+        rowSums = Array(6, 1, 4, 1, 3, 2, 3, 2, 4, 0, 5, 1, 4, 2, 5),
+        colSums = Array(4, 2, 3, 3, 1, 4, 1, 6, 0, 3, 3, 3, 2, 3, 5)
       )
     }
 
     it("TentsAndTrees B - 18x18") {
       tentsAndTreesTest(
-        Array(
+        inputTable = Array(
           " t   Tt T TtTtT T ",
           " T Tt   t     t t ",
           "t              Ttt",
@@ -197,8 +197,8 @@ class AcceptanceTests extends FunSpec with Matchers {
           " T   T tT t   t t ",
           " ttT t     TtTt T "
         ).map(_.toCharArray),
-        Array(6, 2, 1, 7, 1, 4, 4, 4, 3, 5, 3, 3, 4, 2, 4, 3, 3, 4),
-        Array(4, 4, 3, 5, 1, 5, 2, 3, 4, 2, 3, 4, 2, 5, 3, 5, 3, 5)
+        rowSums = Array(6, 2, 1, 7, 1, 4, 4, 4, 3, 5, 3, 3, 4, 2, 4, 3, 3, 4),
+        colSums = Array(4, 4, 3, 5, 1, 5, 2, 3, 4, 2, 3, 4, 2, 5, 3, 5, 3, 5)
       )
     }
   }
